e86ff078ec4829473aba91c1b73a7b9b8ada0f35
Joshua Morony <joshua.morony@gmail.com>
2023-09-18 20:36:21 +0930

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+    ]),
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularstart-chat',
+    appId: '1:767794269558:web:819cea854c7b1b4ce6fe57',
+    storageBucket: 'angularstart-chat.appspot.com',
+    apiKey: 'AIzaSyAmGGkG67K2cZ2H288DWQ46f5TIdIl3vs8',
+    authDomain: 'angularstart-chat.firebaseapp.com',
+    messagingSenderId: '767794269558',
+  },};
