7f1315263b7e537f1864cac62d9390d8d47c497b
Joshua Morony <joshua.morony@gmail.com>
2023-09-22 14:14:46 +0930

    feat: add route for home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component'),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
