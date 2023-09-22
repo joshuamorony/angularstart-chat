54be20f44e8cee06692567b03276db7d04132a0d
Joshua Morony <joshua.morony@gmail.com>
2023-09-22 14:20:18 +0930

    test: should pass value from send event to add$ source

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,60 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import HomeComponent from './home.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MessageService } from '../shared/data-access/message.service';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+  let messageService: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HomeComponent],
+      providers: [
+        {
+          provide: MessageService,
+          useValue: {
+            add$: {
+              next: jest.fn(),
+            },
+          },
+        },
+      ],
+    })
+      .overrideComponent(HomeComponent, {
+        remove: { imports: [] },
+        add: { imports: [] },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    messageService = TestBed.inject(MessageService);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('app-message-input', () => {
+    let messageInput: DebugElement;
+
+    beforeEach(() => {
+      const messageInput = fixture.debugElement.query(
+        By.css('app-message-input')
+      );
+    });
+
+    describe('output: send', () => {
+      it('should next the add$ source on the message service with value', () => {
+        const testValue = 'hello';
+        messageInput.triggerEventHandler('send', testValue);
+
+        expect(messageService.add$.next).toHaveBeenCalledWith(testValue);
+      });
+    });
+  });
+});
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: ` <p>Hello world</p> `,
+})
+export default class HomeComponent {}
