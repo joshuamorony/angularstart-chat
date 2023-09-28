e9876225f36605bf3d81ccd3acb8559fd2cb7559
Joshua Morony <joshua.morony@gmail.com>
2023-09-28 09:36:28 +0930

    feat: display messages in message list

--- src/app/home/ui/message-list.component.ts
+++ src/app/home/ui/message-list.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Message } from 'src/app/shared/interfaces/message';
 
 @Component({
   standalone: true,
   selector: 'app-message-list',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <ul>
+      <li
+        *ngFor="let message of messages; trackBy: trackByFn"
+        data-testid="message"
+      >
+        {{ message.author }}
+        {{ message.content }}
+        {{ message.created }}
+      </li>
+    </ul>
+  `,
+  imports: [CommonModule],
 })
 export class MessageListComponent {
   @Input({ required: true }) messages!: Message[];
+
+  trackByFn(index: number, message: Message) {
+    return message.created;
+  }
 }
