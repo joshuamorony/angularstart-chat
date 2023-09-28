a90fb55cad9adfc31ba8b6a529c5c57fc8ba9ff2
Joshua Morony <joshua.morony@gmail.com>
2023-09-28 09:40:53 +0930

    feat: supply messages to list component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { MessageInputComponent } from './ui/message-input.component';
 import { MessageService } from '../shared/data-access/message.service';
+import { MessageListComponent } from './ui/message-list.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  template: ` <app-message-input (send)="messageService.add$.next($event)" /> `,
-  imports: [MessageInputComponent],
+  template: `
+    <app-message-list [messages]="messageService.messages()" />
+    <app-message-input (send)="messageService.add$.next($event)" />
+  `,
+  imports: [MessageInputComponent, MessageListComponent],
 })
 export default class HomeComponent {
   messageService = inject(MessageService);
