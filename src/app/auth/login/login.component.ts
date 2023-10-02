6891538d27219cdc5139c3bce4a6ca0d330afd60
Joshua Morony <joshua.morony@gmail.com>
2023-10-03 09:17:15 +1030

    feat: link to register page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  template: ` <p>Hello world</p> `,
+  template: ` <a routerLink="/auth/register">Create account</a> `,
+  imports: [RouterModule],
 })
 export class LoginComponent { }
