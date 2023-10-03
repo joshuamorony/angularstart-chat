3184c610976048cc96954e514c81a24900b15ad1
Joshua Morony <joshua.morony@gmail.com>
2023-10-03 20:41:25 +1030

    feat: add user to auth state

--- src/app/shared/data-access/auth.service.ts
+++ src/app/shared/data-access/auth.service.ts
@@ -1,15 +1,37 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { from, defer } from 'rxjs';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { User, createUserWithEmailAndPassword } from 'firebase/auth';
 import { authState } from 'rxfire/auth';
 import { Credentials } from '../interfaces/credentials';
 import { AUTH } from 'src/app/app.config';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+interface AuthState {
+  user: User | null;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   auth = inject(AUTH);
+
+  // sources
+  private user$ = authState(this.auth);
+
+  // state
+  private state = signal<AuthState>({
+    user: null,
+  });
+
+  // selectors
+  user = computed(() => this.state().user);
+
+  constructor() {
+    this.user$
+      .pipe(takeUntilDestroyed())
+      .subscribe((user) => this.state.update((state) => ({ ...state, user })));
+  }
 
   createAccount(credentials: Credentials) {
     return from(
