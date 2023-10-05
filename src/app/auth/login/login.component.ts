efa8814577ce0341a57cdcf47a32e6723fb0763b
Joshua Morony <joshua.morony@gmail.com>
2023-10-05 14:19:16 +1030

    feat: add login form to login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginFormComponent } from './ui/login-form.component';
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  template: ` <a routerLink="/auth/register">Create account</a> `,
-  imports: [RouterModule],
+  template: `
+    <app-login-form loginStatus="pending" />
+    <a routerLink="/auth/register">Create account</a>
+  `,
+  imports: [RouterModule, LoginFormComponent],
 })
-export class LoginComponent { }
+export class LoginComponent {}
