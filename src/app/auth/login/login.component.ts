dd8b2bf69d4d0660346723e79cabc4dbd3c8c589
Joshua Morony <joshua.morony@gmail.com>
2023-10-05 14:48:48 +1030

    feat: use login service in login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, effect, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './ui/login-form.component';
+import { LoginService } from './data-access/login.service';
 
 @Component({
   standalone: true,
   selector: 'app-login',
   template: `
-    <app-login-form loginStatus="pending" />
+    <app-login-form [loginStatus]="loginService.status()" />
     <a routerLink="/auth/register">Create account</a>
   `,
+  providers: [LoginService],
   imports: [RouterModule, LoginFormComponent],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public loginService = inject(LoginService);
+  private router = inject(Router);
+
+  constructor() {
+    effect(() => {
+      if (this.loginService.status() === 'success') {
+        this.router.navigate(['home']);
+      }
+    });
+  }
+}
