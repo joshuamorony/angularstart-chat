3b50ad7fbc86f98223aa01a9b4f719a1a791719e
Joshua Morony <joshua.morony@gmail.com>
2023-10-05 18:53:14 +1030

    fix: remove ngFor

--- src/app/home/ui/message-list.component.ts
+++ src/app/home/ui/message-list.component.ts
@@ -7,22 +7,17 @@ import { Message } from 'src/app/shared/interfaces/message';
   selector: 'app-message-list',
   template: `
     <ul>
-      <li
-        *ngFor="let message of messages; trackBy: trackByFn"
-        data-testid="message"
-      >
+      @for (message of messages; track message.created){
+      <li>
         {{ message.author }}
         {{ message.content }}
         {{ message.created }}
       </li>
+      }
     </ul>
   `,
   imports: [CommonModule],
 })
 export class MessageListComponent {
   @Input({ required: true }) messages!: Message[];
-
-  trackByFn(index: number, message: Message) {
-    return message.created;
-  }
 }
