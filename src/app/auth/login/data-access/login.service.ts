fbd5b5623262dcc4397e8fb14a96dbb33b111726
Joshua Morony <joshua.morony@gmail.com>
2023-10-05 14:45:55 +1030

    feat: create login service

--- src/app/auth/login/data-access/login.service.ts
+++ src/app/auth/login/data-access/login.service.ts
@@ -1,1 +1,61 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { EMPTY, Subject, switchMap } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from 'src/app/shared/data-access/auth.service';
+import { Credentials } from 'src/app/shared/interfaces/credentials';
+
 export type LoginStatus = 'pending' | 'authenticating' | 'success' | 'error';
+
+interface LoginState {
+  status: LoginStatus;
+}
+
+@Injectable()
+export class LoginService {
+  private authService = inject(AuthService);
+
+  // sources
+  error$ = new Subject<any>();
+  login$ = new Subject<Credentials>();
+
+  userAuthenticated$ = this.login$.pipe(
+    switchMap((credentials) =>
+      this.authService.login(credentials).pipe(
+        catchError((err) => {
+          this.error$.next(err);
+          return EMPTY;
+        })
+      )
+    )
+  );
+
+  // state
+  private state = signal<LoginState>({
+    status: 'pending',
+  });
+
+  // selectors
+  status = computed(() => this.state().status);
+
+  constructor() {
+    // reducers
+    this.userAuthenticated$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, status: 'success' }))
+      );
+
+    this.login$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, status: 'authenticating' }))
+      );
+
+    this.error$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, status: 'error' }))
+      );
+  }
+}
