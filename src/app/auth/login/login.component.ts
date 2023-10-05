c565fb2f2ed26bb848ddb09b9d02b832d1b7fa66
Joshua Morony <joshua.morony@gmail.com>
2023-10-05 15:17:26 +1030

    fix: use authState for effect to persist login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, effect, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './ui/login-form.component';
 import { LoginService } from './data-access/login.service';
+import { AuthService } from 'src/app/shared/data-access/auth.service';
 
 @Component({
   standalone: true,
@@ -18,11 +19,12 @@ import { LoginService } from './data-access/login.service';
 })
 export class LoginComponent {
   public loginService = inject(LoginService);
+  private authService = inject(AuthService);
   private router = inject(Router);
 
   constructor() {
     effect(() => {
-      if (this.loginService.status() === 'success') {
+      if (this.authService.user()) {
         this.router.navigate(['home']);
       }
     });
