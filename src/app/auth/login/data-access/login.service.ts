f84fa6417fb4039c08e98c546455c079abac4c49
Joshua Morony <joshua.morony@gmail.com>
2023-10-22 22:33:12 +1030

    refactor: login simple approach

--- src/app/auth/login/data-access/login.service.ts
+++ src/app/auth/login/data-access/login.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { EMPTY, Subject, switchMap } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { AuthService } from 'src/app/shared/data-access/auth.service';
 import { Credentials } from 'src/app/shared/interfaces/credentials';
+import { connect } from 'ngxtension/connect';
 
 export type LoginStatus = 'pending' | 'authenticating' | 'success' | 'error';
 
@@ -40,22 +41,19 @@ export class LoginService {
 
   constructor() {
     // reducers
-    this.userAuthenticated$
-      .pipe(takeUntilDestroyed())
-      .subscribe(() =>
-        this.state.update((state) => ({ ...state, status: 'success' }))
-      );
+    connect(
+      this.state,
+      this.userAuthenticated$.pipe(map(() => ({ status: 'success' as const })))
+    );
 
-    this.login$
-      .pipe(takeUntilDestroyed())
-      .subscribe(() =>
-        this.state.update((state) => ({ ...state, status: 'authenticating' }))
-      );
+    connect(
+      this.state,
+      this.login$.pipe(map(() => ({ status: 'authenticating' as const })))
+    );
 
-    this.error$
-      .pipe(takeUntilDestroyed())
-      .subscribe(() =>
-        this.state.update((state) => ({ ...state, status: 'error' }))
-      );
+    connect(
+      this.state,
+      this.error$.pipe(map(() => ({ status: 'error' as const })))
+    );
   }
 }
