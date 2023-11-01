7f9f3c3c7049b6714902171575204b56e851d1c2
Enea Jahollari <jahollarienea14@gmail.com>
2023-11-01 23:08:28 +0100

    update lib

--- src/app/shared/data-access/message.service.ts
+++ src/app/shared/data-access/message.service.ts
@@ -24,7 +24,6 @@ export class MessageService {
 
   actions = rxActions<{ add: Message['content'] }>();
 
-  // @ts-ignore TODO: remove @ts-ignore when new rx-angular/state version is released
   state = rxState<MessageState>(({set, connect, select}) => {
     set({ messages: [], error: null });
     connect('authUser', this.authService.user);
