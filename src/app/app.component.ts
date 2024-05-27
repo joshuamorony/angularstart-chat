0793dae44abf00bdcfe8390d0301f15a39a5ba32
Joshua Morony <joshua.morony@gmail.com>
2024-05-27 09:42:01 +0930

    refactor: remove common module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   template: ` <router-outlet></router-outlet> `,
   styles: [],
 })
