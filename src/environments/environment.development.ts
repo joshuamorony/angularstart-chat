e8fc4c8386934acb95f4dfa230ccd4f109c31e57
Joshua Morony <joshua.morony@gmail.com>
2025-05-29 13:20:59 +0930

    v20 migration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
   firebase: {
     projectId: 'demo-project',
-    appId: '1:767794269558:web:819cea854c7b1b4ce6fe57',
+    appId: '*************',
     storageBucket: 'angularstart-chat.appspot.com',
-    apiKey: 'AIzaSyAmGGkG67K2cZ2H288DWQ46f5TIdIl3vs8',
+    apiKey: '*******************',
     authDomain: 'angularstart-chat.firebaseapp.com',
     messagingSenderId: '767794269558',
   },
